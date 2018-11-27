04bd7ea61d58be5c79220f4adc0e0e414fcfac66
Unknown <mrrojano97@icloud.com>
2018-11-27 18:14:14 -0300

    Se añade clase puja

---
+++ src/Puja.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author elias
+ */
+public class Puja {
+    private double dinero;
+    private String nombreUsuario;
+
+    public Puja(double dinero, String nombreUsuario) {
+        this.dinero = dinero;
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public double getDinero() {
+        return dinero;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+    
+    
+}
